033482e9923fbca3f7815b6cf80dcf9445f00e20
Ivan Bagrin <bagrinivanray@gmail.com>
2021-08-02 01:07:43 +0300

    Search Content + Cards Content

--- src/Components/Body/Card/Cards.jsx
+++ src/Components/Body/Card/Cards.jsx
@@ -1,10 +1,55 @@
 import styled from "styled-components";
+import {Hentai} from "../../../store/q=Boku_Genre=12";
+import Shape from '../../../Shape.svg'
 
-export const Card = () => {
+export const Card = ({img, title, descr}) => {
+  const cardImg = Hentai.results[0].image_url;
+  const cardTitle = Hentai.results[0].title;
+  const cardDescr = Hentai.results[0].synopsis;
     return <CardWrapper>
-
-    </CardWrapper>
+      <CardWrapperBlock>
+        <CardImg src={cardImg}/>
+        <CardWrapperDescr>
+          <CardTitle> {cardTitle} </CardTitle>
+          <CardDescr> {cardDescr} </CardDescr>
+        </CardWrapperDescr>
+        
+      </CardWrapperBlock>
+        <CardShape src = {Shape} />
+      </CardWrapper>
 }
 const CardWrapper = styled.div`
-  background: green`
+  background: green;
+  padding: 40px 60px 40px 40px;
+  `
+
+const CardImg = styled.img`
+  
+  width: 200px;
+  margin-right: 29px;
+  `
+
+const CardTitle = styled.div `
+  font-size: 36px;
+  color: #000000;
+`
+
+const CardDescr = styled.div `
+  font-size: 18px;
+  line-height: 21px;
+  margin-top: 20px;
+  color: #000000;
+`
+
+const CardWrapperBlock = styled.div `
+  display: grid;
+  grid-template-columns: minmax(0, auto) 1fr;`
+
+const CardShape = styled.img `
+  margin-top: 30px;
+  cursor: pointer;`
+  
+const CardWrapperDescr = styled.div ``
+
+
 
--- src/Components/Body/Search.jsx
+++ src/Components/Body/Search.jsx
@@ -1,21 +1,47 @@
 import styled from "styled-components";
 import {Categories} from "./Categories/Categories";
 import {Genre} from "./Categories/Genre";
-
+import {Result} from "./Result/Result"
+import {Hentai} from "../../store/q=Boku_Genre=12";
+import {Yaoi} from "../../store/q=6loverGenre=33";
 export const Search = () => {
     return <SearchWrapper>
-	   <Input/>
+	    <Input/>
+        <ResultsBlock>
+          {Hentai.results.map(item => {
+            return <Result 
+              key = {Math.random()}
+              img = {item.image_url}
+              title = {item.title}
+              synopsis = {item.synopsis}
+            />
+          })}
+
+          {Yaoi.results.map(item => {
+            return <Result 
+              key = {Math.random()}
+              img = {item.image_url}
+              title = {item.title}
+              synopsis = {item.synopsis}
+            />
+          })} 
+        </ResultsBlock>
     </SearchWrapper>
 }
 const SearchWrapper = styled.div`
   background: coral;
-overflow-x: hidden;
-height: 85vh;`
+  overflow-x: hidden;
+  height: 85vh;`
 
 
 const Input = styled.input`
-width: 100%;
-height: 50px;
-font-family: Roboto;
-font-size: 30px;
+  width: 100%;
+  height: 50px;
+  font-family: Roboto;
+  font-size: 30px;
 `
+
+const ResultsBlock = styled.div `
+  
+`
+
