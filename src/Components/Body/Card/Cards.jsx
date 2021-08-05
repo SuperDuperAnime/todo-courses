6e5a8214f8e0012673f9b7e401efdedabb274c31
Aleksandr <skelikDev@gmail.com>
2021-08-05 17:27:16 +0300

    mobX/axios
    you can get api search with your inputVlaue

--- src/Components/Body/Card/Cards.jsx
+++ src/Components/Body/Card/Cards.jsx
@@ -3,54 +3,57 @@ import {Hentai} from "../../../store/q=Boku_Genre=12";
 import Shape from '../../../Shape.svg'
 
 export const Card = ({img, title, descr, isFavorite}) => {
-  const cardImg = Hentai.results[0].image_url;
-  const cardTitle = Hentai.results[0].title;
-  const cardDescr = Hentai.results[0].synopsis;
-    return <CardWrapper>
-      <CardWrapperBlock>
-        <CardImg src={cardImg}/>
-        <CardWrapperDescr>
-          <CardTitle> {cardTitle} </CardTitle>
-          <CardDescr> {cardDescr} </CardDescr>
-        </CardWrapperDescr>
-        
-      </CardWrapperBlock>
-        <CardShape src = {Shape} />
-      </CardWrapper>
+	   const cardImg = Hentai.results[0].image_url;
+	   const cardTitle = Hentai.results[0].title;
+	   const cardDescr = Hentai.results[0].synopsis;
 
-}
+
+	   return <CardWrapper>
+		  <CardWrapperBlock>
+			 <CardImg src={cardImg}/>
+			 <CardWrapperDescr>
+				<CardTitle> {cardTitle} </CardTitle>
+				<CardDescr> {cardDescr} </CardDescr>
+			 </CardWrapperDescr>
+
+		  </CardWrapperBlock>
+		  <CardShape src={Shape}/>
+	   </CardWrapper>
+
+    }
+
 const CardWrapper = styled.div`
   background: green;
   padding: 40px 60px 40px 40px;
-  `
+`
 
 const CardImg = styled.img`
-  
+
   width: 200px;
   margin-right: 29px;
-  `
+`
 
-const CardTitle = styled.div `
+const CardTitle = styled.div`
   font-size: 36px;
   color: #000000;
 `
 
-const CardDescr = styled.div `
+const CardDescr = styled.div`
   font-size: 18px;
   line-height: 21px;
   margin-top: 20px;
   color: #000000;
 `
 
-const CardWrapperBlock = styled.div `
+const CardWrapperBlock = styled.div`
   display: grid;
   grid-template-columns: minmax(0, auto) 1fr;`
 
-const CardShape = styled.img `
+const CardShape = styled.img`
   margin-top: 30px;
   cursor: pointer;`
-  
-const CardWrapperDescr = styled.div ``
+
+const CardWrapperDescr = styled.div``
 
 
 
