dde6f70f83d427c8fca878fbc62f02d74acc2202
Ivan Bagrin <bagrinivanray@gmail.com>
2021-08-07 16:33:26 +0300

    image Content

--- src/Components/EnterComponents/Content.jsx
+++ src/Components/EnterComponents/Content.jsx
@@ -2,28 +2,28 @@ import React from 'react';
 import {Box} from "rebass/styled-components";
 import Shape from "../../Shape.svg";
 import styled from "styled-components";
+import store from "../../store/store";
+import {observer} from "mobx-react-lite";
 import {Hentai} from "../../store/q=Boku_Genre=12";
 
-export const Content = ({img, title, descr,}) => {
-    const cardImg = Hentai.results[0].image_url;
-    const cardTitle = Hentai.results[0].title;
-    const cardDescr = Hentai.results[0].synopsis;
-    return (
-        <Box width={"100%"} height={"100%"} bg={'rgba(0,255,0,0.3)'}>
-            <CardWrapper>
+export const Content = observer(() => {
+  const content = store.content===null ? null : 
+  <CardWrapper>
                 <CardWrapperBlock>
-                    <CardImg src={cardImg}/>
+                    <CardImg src={store.content.image_url}/>
                     <CardWrapperDescr>
-                        <CardTitle> {cardTitle} </CardTitle>
-                        <CardDescr> {cardDescr} </CardDescr>
+                        <CardTitle> {store.content.title || store.content.name} </CardTitle>
+                        <CardDescr> {store.content.synopsis || store.content.alternative_names} </CardDescr>
                     </CardWrapperDescr>
-
                 </CardWrapperBlock>
                 <CardShape src={Shape}/>
             </CardWrapper>
+    return (
+        <Box width={"100%"} height={"100%"} bg={'rgba(0,255,0,0.3)'}>
+            {content}
         </Box>
     );
-};
+});
 
 const CardWrapper = styled.div`
   background: green;
